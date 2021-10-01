d31a8483cf4e67471e498915d4947f525ca3b6e7
Melanie Gunn <mgunn589@gmail.com>
2021-10-01 09:34:12 -0500

    added bootstrap as a dependency

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import PlaygroundDetails from './PlaygroundDetails'
+import registerServiceWorker from './registerServiceWorker';
 
 
-ReactDOM.render(<PlaygroundDetails />, document.getElementById('root'));
+
+ReactDOM.render(<PlaygroundDetails />, document.getElementById('root')); registerServiceWorker();
 
