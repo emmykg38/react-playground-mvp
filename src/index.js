deebe092010f68cea0d71c849c2c53d3b2dde7a0
Melanie Gunn <mgunn589@gmail.com>
2021-10-04 14:20:52 -0500

    bootstrap functional

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import PlaygroundDetails from './PlaygroundDetails'
-import registerServiceWorker from './registerServiceWorker';
 
 
 
-ReactDOM.render(<PlaygroundDetails />, document.getElementById('root')); registerServiceWorker();
+
+ReactDOM.render(<PlaygroundDetails />, document.getElementById('root')); 
 
